6a869d36a3c68c45728ddb76d53c6b5c0b6d96c3
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-12-06 00:45:24 +0200

    (0) made the utils/redis file with the RedisClient class

---
+++ utils/redis.js
@@ -0,0 +1,43 @@
+import { createClient } from 'redis';
+import util from "util";
+
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isConnected = true;
+    this.client.on('error', (error)=> {
+      this.isConnected = false;
+      console.log(error);
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    const getAsync = util.promisify(this.client.get).bind(this.client);
+    const ret = getAsync(key);
+    ret.catch(console.error);
+    return ret;
+  }
+
+  async set(key, value, duration) {
+    const setAsync = util.promisify(this.client.set).bind(this.client);
+    const ret = setAsync(key, value, 'EX', 10);
+    ret.catch(console.error);
+    return ret;
+  }
+
+  async del(key) {
+    const delAsync = util.promisify(this.client.del).bind(this.client);
+    const ret = delAsync(key);
+    ret.catch(console.error);
+    return ret;
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
+
