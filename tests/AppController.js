567c496aef96e9824ca9fc863bcd4df0830d51a4
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-12-06 11:11:35 +0200

    get/stats tested

--- tests/AppController.js
+++ tests/AppController.js
@@ -7,16 +7,15 @@ import sinon from "sinon";
 chai.use(chaiHttp);
 
 describe('Testing AppController', function() {
-
-  let stub;
-  let stub2;
-
-  afterEach(function () {
-    stub.restore();
-    stub2.restore()
-  });
-
   describe('GET /status', () => {
+    let stub;
+    let stub2;
+
+    afterEach(function () {
+      stub.restore();
+      stub2.restore()
+    });
+
     it('returns true when database is alive', (done) => {
       stub = sinon.stub(db, 'isAlive').returns(true);
       stub2 = sinon.stub(redisDB, 'isAlive').returns(true);
@@ -42,4 +41,22 @@ describe('Testing AppController', function() {
       });
     });
   });
+  describe('GET /stats', () => {
+    it ('returns amount of users and files',(done) => {
+      let stub = sinon.stub(db, 'nbUsers').returns(5);
+      let stub2 = sinon.stub(db, 'nbFiles').returns(10);
+      chai.request(app).get('/stats').end((err, res) => {
+        if (err) return done(err);
+        expect(res).to.have.status(200);
+        expect(res).to.be.an('object');
+        expect(res.body.users).to.be.equal(5);
+        expect(res.body.files).to.be.equal(10);
+        stub.restore();
+        stub2.restore();
+        done();
+      });
+    });
+  });
 });
+
+
