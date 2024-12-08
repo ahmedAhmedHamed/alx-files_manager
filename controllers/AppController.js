f09039200c552e209dc70416af91b54bb8b8ba23
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-12-08 04:08:18 +0200

    lint controllers/AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,16 +1,17 @@
 import db from '../utils/db';
 import redisDB from '../utils/redis';
+
 module.exports = (app) => {
   app.get('/status', (req, res) => {
     res.status(200).json({
-      'redis': redisDB.isAlive(),
-      'db': db.isAlive()
+      redis: redisDB.isAlive(),
+      db: db.isAlive(),
     });
   });
   app.get('/stats', async (req, res) => {
     res.status(200).json({
-     'users': await db.nbUsers(),
-     'files': await db.nbFiles()
+      users: await db.nbUsers(),
+      files: await db.nbFiles(),
     });
   });
-}
+};
