ff09579c2dd5006dcf68fa9336a7fed34674294e
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-12-08 04:07:00 +0200

    lint index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import appControllerPaths from '../controllers/AppController';
 import userControllerPaths from '../controllers/UsersController';
 import authenticationControllerPaths from '../controllers/AuthController';
+
 module.exports = (app) => {
   appControllerPaths(app);
   userControllerPaths(app);
