aa5401923d72f41c44bcce80853e44d3c8c8198d
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-12-08 04:22:30 +0200

    lint controllers/AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,35 +1,35 @@
+import { v4 as uuidv4 } from 'uuid';
 import db from '../utils/db';
 import redisDB from '../utils/redis';
-import { v4 as uuidv4 } from 'uuid';
 import queries from '../utils/queries';
+
 module.exports = (app) => {
   app.get('/connect', async (req, res) => {
     const authheader = req.headers.authorization;
     if (!authheader) {
-      return res.status(401).json({'error': 'Unauthorized'});
+      return res.status(401).json({ error: 'Unauthorized' });
     }
     let email = false;
     let password = false;
     try {
-      const auth = new Buffer.from(authheader.split(' ')[1],
+      /* eslint-disable new-cap */
+      [email, password] = new Buffer.from(authheader.split(' ')[1],
         'base64').toString().split(':');
-      email = auth[0];
-      password = auth[1];
     } catch (e) {
-      return res.status(401).json({'error': 'Unauthorized'});
+      return res.status(401).json({ error: 'Unauthorized' });
     }
     if (!email || !password) {
-      return res.status(401).json({'error': 'Unauthorized'});
+      return res.status(401).json({ error: 'Unauthorized' });
     }
-    const user = await db.getUserFull(email, password)
+    const user = await db.getUserFull(email, password);
     if (!user) {
-      return res.status(401).json({'error': 'Unauthorized'});
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    const sessionToken =  uuidv4();
-    await redisDB.set('auth_' + sessionToken, email, 24 * 60 * 60)
+    const sessionToken = uuidv4();
+    await redisDB.set(`auth_ + ${sessionToken}`, email, 24 * 60 * 60);
     return res.status(200).json(
-      {'token': sessionToken}
+      { token: sessionToken },
     );
   });
 
@@ -37,9 +37,9 @@ module.exports = (app) => {
     const authHeader = req.get('X-Token');
     const user = await queries.getUserFromHeader(authHeader);
     if (!user) {
-      return res.status(401).json({'error': 'Unauthorized'});
+      return res.status(401).json({ error: 'Unauthorized' });
     }
-    await redisDB.del('auth_' + authHeader);
+    await redisDB.del(`auth_ + ${authHeader}`);
     return res.status(204).send();
-  })
-}
+  });
+};
