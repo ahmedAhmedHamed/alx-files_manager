7359470e3c338669a3f249bdf63b33b05e75f085
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-12-08 16:55:23 +0200

    (5) FilesController.js - file

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,9 +1,12 @@
 import fileUtils from '../utils/file';
 import queries from "../utils/queries";
+import { v4 as uuidv4 } from 'uuid';
+
 module.exports = (app) => {
   const allowedTypes = ['folder', 'file', 'image'];
   app.post('/files', async (req, res) => {
     const authHeader = req.get('X-Token');
+    const storingFolder = process.env.FOLDER_PATH || '/tmp/files_manager';
     const user = await queries.getUserFromHeader(authHeader);
     if (!user) {
       return res.status(401).json({'error': 'Unauthorized'});
@@ -15,21 +18,38 @@ module.exports = (app) => {
     const isPublic = body.isPublic || false;
     const data = body.data;
     if (!filename) {
-      return res.status(400).json({'error': 'Missing name'});
+      return res.status(400).json({error: 'Missing name'});
     }
     if (!type || !allowedTypes.includes(type)) {
-      return res.status(400).json({'error': 'Missing type'});
+      return res.status(400).json({error: 'Missing type'});
     }
     if (!data && type !== 'folder') {
-      return res.status(400).json({'error': 'Missing data'});
+      return res.status(400).json({error: 'Missing data'});
     }
-
+    if (parentId !== 0) {
+      const parentCheck = await fileUtils.getFileFromId(parentId);
+      if (!parentCheck) {
+        return res.status(400).json({error: 'Parent not found'});
+      }
+      if (parentCheck.type !== 'folder') {
+        return res.status(400).json({error: 'Parent is not a folder'});
+      }
+    }
     if (type === 'folder') {
       fileUtils.addFolder(user._id.toString(), filename, parentId, isPublic).then((result) => {
         const ret = { ...result.ops[0] };
         ret.id = ret._id.toString();
         delete ret._id;
-        res.status(201).json(ret)
+        return res.status(201).json(ret);
+      });
+    } else {
+      const filePath = fileUtils.createFile(storingFolder, uuidv4(), data);
+      fileUtils.addFile(user._id.toString(), filename, parentId, isPublic, filePath, type, data).then((result) => {
+        const ret = { ...result.ops[0] };
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.localPath;
+        return res.status(201).json(ret);
       });
     }
 
--- utils/file.js
+++ utils/file.js
@@ -1,4 +1,6 @@
 import db from './db';
+import fs from 'fs';
+import {ObjectId} from 'mongodb';
 
 class FileUtils {
   constructor(){}
@@ -6,6 +8,29 @@ class FileUtils {
   addFolder(userId, name, parentId, isPublic) {
     return db.filesCollection.insertOne({ userId, name, parentId, isPublic,
     type: 'folder' });
+  }
+
+  addFile(userId, name, parentId, isPublic, localPath, type, data) {
+    return db.filesCollection.insertOne({ userId, name, parentId, isPublic,
+    type, localPath, data });
+  }
+
+  createFile(storingFolder, filename, base64Data) {
+    if (!fs.existsSync(storingFolder)) {
+    fs.mkdirSync(storingFolder, { recursive: true });
+    }
+    const filePath = `${storingFolder}/${filename}`;
+    const buffer = Buffer.from(base64Data, 'base64');
+    fs.writeFileSync(filePath, buffer);
+    return filePath;
+  }
+
+  getFileFromId(id) {
+    try {
+      return db.filesCollection.findOne({ _id: ObjectId(id) });
+    } catch (err) {
+      return false;
+    }
   }
 }
 
