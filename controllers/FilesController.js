5ec07206bf1237fc96c15306ece742cb0226ad43
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-12-09 06:49:32 +0200

    (6) rollback of previous

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -64,11 +64,10 @@ module.exports = (app) => {
     if (!document || document.userId !== user._id.toString()) {
       return res.status(404).json({ error: 'Not found' });
     }
-    const ret = { ...document };
-    ret.id = ret._id.toString();
+    const ret = { id: document._id, ...document };
     delete ret._id;
     delete ret.localPath;
-    return res.status(200).json({});
+    return res.status(200).json(ret);
   });
 
   app.get('/files', async (req, res) => {
